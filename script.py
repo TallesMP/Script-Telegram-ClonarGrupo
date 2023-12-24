c6c3b931302fa559d3d4d9dcd7ec269bc894bacc
Talles Martins <125074087+TallesMP@users.noreply.github.com>
2023-12-23 23:46:14 -0300

    Add files via upload

--- script.py
+++ script.py
@@ -3,14 +3,11 @@ from telethon.tl import types
 import time
 import os
 
-api_id = "" # Insira o seu ID da api telegram entre as aspas
-api_hash = "" # Insira a sua hash da api telegram entre as aspas
+api_id = "22420244"
+api_hash = "6b3c34d32dd4156f5c159470190fb1cc"
 
-grupo_origem = 0 # Digite a ID do grupo que deseja clonar
-grupo_destino = 0 # Digite a ID do grupo clonado
-
-tamanho_lote = 10000
-msg_enviada = 0
+grupo_origem = -1001276169293
+grupo_destino = -4040633986
 
 def escreverCache(txt):
     with open("mensagemIdCache", "w") as cache:
@@ -28,15 +25,11 @@ while True:
         with TelegramClient('clonarCanais', api_id, api_hash) as usuario:
             # Seleciona a mensagem
             msg = usuario.get_messages(grupo_origem, limit=1, reverse=True, offset_id = lerCache())[0]
-            msg_atual = msg.id
-            escreverCache(msg_atual)
+            escreverCache(msg.id)
+            # Envia apenas se houver um tipo de midia anexado
             if not isinstance(msg, types.MessageService) and hasattr(msg, 'media') and (isinstance(msg.media, types.MessageMediaDocument) or isinstance(msg.media, types.MessageMediaPhoto)):
                 usuario.forward_messages(grupo_destino, msg)
                 print(msg.id)
-                #msg_enviada += 1
-                #if msg_enviada % 1999 == 0: 
-                #    msg_enviada = 0
-                #    time.sleep(2000)
     except Exception as erro:
         print(f"ERRO: {erro}")
         time.sleep(120)
